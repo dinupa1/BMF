a311cff5278c5f93c6c7d8e8b6ee278cecb38cdd
dinupa <dinupa3@gmail.com>
2023-08-09 11:56:34 -0500

    VDM model update

---
+++ BackgroundVDM.py
@@ -0,0 +1,81 @@
+#
+# dinupa3@gmail.com
+# 08-08-2023
+#
+
+import torch
+import torch.nn as nn
+
+# A simple variational diffusion model use for background substraction
+
+
+class BackgroundVDM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(BackgroundVDM, self).__init__()
+
+        # Encoder layers
+        self.fc_encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim, bias=True),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim, bias=True),
+            nn.ReLU(),
+            )
+
+        self.fc_mu = nn.Linear(hidden_dim, latent_dim, bias=True)
+        self.fc_logvar = nn.Linear(hidden_dim, latent_dim, bias=True)
+
+
+        # Decoder layers
+        self.fc_decoder = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim, bias=True),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim, bias=True),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, input_dim, bias=True),
+            )
+
+    def encode(self, x):
+        h = self.fc_encoder(x)
+        mu, logvar = self.fc_mu(h), self.fc_logvar(h)
+        return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5* logvar)
+        eps = torch.randn_like(std)
+        return mu + eps* std
+
+    def decode(self, z):
+        h = self.fc_decoder(z)
+        return h
+
+    def forward(self, x, timesteps):
+
+        # Encode
+        mu, logvar = self.encode(x)
+
+        # Reparameterize and sample from the latent space
+        z = self.reparameterize(mu, logvar)
+
+        # Perform diffusion process
+        for t in range(timesteps):
+            z_new = self.decode(z)
+            diffusion_noise = torch.randn_like(z_new)
+            z_new = z + torch.sqrt(0.1)* z_new * diffusion_noise # Diffusion step
+            z = z_new
+
+        # Final output
+        reco_x = self.decoder(z)
+
+        return reco_x, mu, logvar
+
+
+class VDMLoss(nn.Module):
+    def __init__(self):
+        super(VDMLoss, self).__init__()
+
+    def forward(self, reco_x, x, batch_weight, mu, logvar):
+        mse_loss = nn.MSELoss(reduction="sum")
+        MSE = mse_loss(reco_x, x)
+        MSE_weight = torch.sum(batch_weight*MSE)
+        KLD = torch.sum(1. + logvar - mu.pow(2) - logvar.exp())
+        return (MSE_weight + KLD)/x.size(0)
---
+++ SimpleVDM.py
@@ -0,0 +1,59 @@
+#
+# dinupa3@gmail.com
+# 08-08-2023
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+import uproot
+import awkward as ak
+
+from BackgroundVDM import BackgroundVDM
+from BackgroundVDM import VDMLoss
+
+# Some conts.
+batch_size = 1000
+num_epochs = 100
+
+# Load E906 LH2 data
+tree = uproot.open("LH2Data.root:tree")
+events = tree.arrays(["mass", "pT", "xF", "phi", "costh", "weight"])
+
+X = np.array([(mass, pT, xF, phi, costh) for mass, pT, xF, phi, costh in zip(events.mass, events.pT, events.xF, events.phi, events.costh)])
+weight = np.array([(weight) for weight in events.weight]).reshape(-1, 1)
+
+# Convert to torch tensor
+X_tensor = torch.from_numpy(X).float()
+weight_tensor = torch.from_numpy(weight).float()
+
+dataset = TensorDataset(X_tensor, weight_tensor)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# Create the VDM
+input_dim = 5
+hidden_dim = 100
+latent_dim = 10
+time_steps = 1000
+vdm = BackgroundVDM(input_dim, hidden_dim, latent_dim)
+
+# Define the optimizer and loss
+optimizer = optim.Adam(vdm.parameters(), lr=0.001)
+loss_vdm = VDMLoss()
+
+# Training loop
+for epoch in range(num_epochs):
+    vdm.train()
+    for batch_data, batch_weight in dataloader:
+        optimizer.zero_grad()
+        reco_data, mu, logvar = vdm(batch_data, time_steps)
+        loss = loss_vdm(reco_data, batch_data, batch_weight, mu, logvar)
+        loss.backward()
+        optimizer.step()
+        if epoch % 10 == 0:
+            print(f"===> Epoch [{epoch}/{num_epochs}], Loss: {loss.item()}")
